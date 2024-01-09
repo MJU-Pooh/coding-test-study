80a2357e7ab2df9fdb341354e1137a21d075216b
fredleeJH <fred54755475@gmail.com>
2024-01-09 18:02:20 +0900

    [백준] python 시험 성적

---
+++ jonghun/python/baekjoon/level2/9498_시험 성적.py
@@ -0,0 +1,13 @@
+#시험성적 9498
+a = int(input())
+if a>=90:
+    print("A")
+elif a>=80:
+    print("B")
+elif a>=70:
+    print("C")
+elif a>=60:
+    print("D")
+else:
+    print("F")
+#이건 너무 수준이 낮다.
