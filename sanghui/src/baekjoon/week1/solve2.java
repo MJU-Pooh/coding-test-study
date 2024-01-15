255f1bf27c1096cc30febe712c82148d7694e488
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-01-15 19:38:26 +0900

    [백준] 자바 2164 카드게임

---
+++ sanghui/src/baekjoon/week1/solve2.java
@@ -0,0 +1,33 @@
+package baekjoon.week1;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class solve2 {
+    public static void main(String[] args) {
+
+
+        Scanner input = new Scanner(System.in);
+
+        int N = input.nextInt();
+
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=1;i<N+1;i++) //큐에 원소 삽입
+        {
+            queue.add(i);
+        }
+
+        while(queue.size()>1)
+        {
+            queue.poll();//버리고
+
+            queue.add(queue.poll());//맨 밑으로 넣는 작업
+
+        }
+
+        System.out.println(queue.poll());
+
+
+    }
+}
